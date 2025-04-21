fced52e2d47725972d9176a3c101219e38bdd667
alexClowell <82552726+ultrafinest@users.noreply.github.com>
2025-04-21 12:47:06 -0700

    StepTracker.java

--- src/main/java/StepTracker.java
+++ src/main/java/StepTracker.java
@@ -3,25 +3,28 @@ public class StepTracker
 {
   private int minSteps;
   private int totalDays;
-  private int activeDays;
+  private int actDays;
   private int totalSteps;
   public StepTracker(int nm){
    minSteps = nm;
    totalDays = 0;
-   activeDays = 0;
+   actDays = 0;
    totalSteps = 0;
   }
  public void addDailySteps(int add){
   totalDays++;
   totalSteps += add;
   if(add >= minSteps){
-   activeDays++;
+   actDays++;
   }
  }
  public int activeDays(){
-  return activeDays;
+  return actDays;
  }
  public double averageSteps(){
+  if(totalDays == 0){
+    return 0.0;
+  }
   return (double)totalSteps / (double)totalDays;
  }
 } 
